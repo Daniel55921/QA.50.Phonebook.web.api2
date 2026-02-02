598a357b20c3f39e7abaf435006a87076c762048
Daniil <manevich95@gmail.com>
2026-02-02 10:36:38 +0200

    Registration Positive Test

--- src/test/java/ui_tests/LoginTests.java
+++ src/test/java/ui_tests/LoginTests.java
@@ -2,7 +2,9 @@ package ui_tests;
 
 import dto.User;
 import manager.AppManager;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.ContactPage;
 import pages.HomePage;
 import pages.LoginPage;
 
@@ -10,13 +12,13 @@ import pages.LoginPage;
 public class LoginTests extends AppManager {
     @Test
     public void loginPositiveTest() {
-        System.out.println("first test");
         HomePage homePage = new HomePage(getDriver());
         homePage.clickBtnLogin();
         LoginPage loginPage = new LoginPage(getDriver());
         loginPage.typeLoginRegistrationForm("cat12322@mail",
                 "Kit088877!!");
         loginPage.clickBtnLoginForm();
+        Assert.assertTrue(new ContactPage(getDriver()).isTextInBtnAddPresent("ADD"));
     }
 
     @Test
@@ -28,6 +30,17 @@ public class LoginTests extends AppManager {
         LoginPage loginPage = new LoginPage(getDriver());
         loginPage.typeLoginRegistrationFormWithUser(user);
         loginPage.clickBtnLoginForm();
+        Assert.assertTrue(new ContactPage(getDriver()).isTextInBtnSignOutPresent("Sign Out"));
+    }
 
+    @Test
+    public void loginNegativeTest_WrongEmail() {
+        User user = new User("cat12322mail", "Kit088877!!");
+        HomePage homePage = new HomePage(getDriver());
+        homePage.clickBtnLogin();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginRegistrationFormWithUser(user);
+        loginPage.clickBtnLoginForm();
+        Assert.assertEquals(loginPage.closeAlertReturnText(), "Wrong email or password");
     }
 }
